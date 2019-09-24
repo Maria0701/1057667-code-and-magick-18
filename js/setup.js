45afe4ecc462fe52a6483eba91d250323a6de959
Maria <reflection2008@gmail.com>
2019-09-24 14:19:59 +0300

    Создана орда

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,46 @@
 'use strict';
+var FRIENDS_NUMBER = 4;
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COAT = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
+var getRandomInteger = function randomInteger(min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min));
+};
+
+var wizards = [];
+for (var j = 0; j < FRIENDS_NUMBER; j++) {
+  wizards[j] = {
+    name: WIZARD_NAMES[getRandomInteger(0, WIZARD_NAMES.length - 1)] + ' ' + WIZARD_SURNAMES[getRandomInteger(0, WIZARD_SURNAMES.length - 1)],
+    coatColor: WIZARD_COAT[getRandomInteger(0, WIZARD_COAT.length - 1)],
+    eyesColor: WIZARD_EYES[getRandomInteger(0, WIZARD_EYES.length - 1)]
+  };
+}
+
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+.content
+.querySelector('.setup-similar-item');
+var fragment = document.createDocumentFragment();
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+};
+
+for (var i = 0; i < FRIENDS_NUMBER; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+
+similarListElement.appendChild(fragment);
