31f015c01eecacd144619c7231fd415e67e04e29
Maria <reflection2008@gmail.com>
2019-10-15 22:20:08 +0300

    Добавлены получение и отправка данных

---
+++ js/backend.js
@@ -0,0 +1,59 @@
+'use strict';
+(function () {
+
+
+
+  var save = function (url, data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.timeout = 10000;
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.open('POST', url);
+    xhr.send(data);
+  };
+
+  var send = function (url, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.timeout = 10000;
+
+    xhr.open('GET', url);
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.send();
+  };
+
+  window.backend = {
+    save: save,
+    load: send
+  };
+})();
--- js/utils.js
+++ js/utils.js
@@ -8,8 +8,32 @@
     return arr[getRandomInteger(0, arr.length - 1)];
   };
 
+  var createNewElement = function (tag, className, text, style) {
+    var newElement = document.createElement(tag);
+    newElement.classList.add(className);
+    if (style) {
+      newElement.setAttribute('style', style);
+    }
+    newElement.textContent = (text);
+    return newElement;
+  };
+
+  var loadErrorHahdler = function (message) {
+    var mistake = window.utils.createNewElement('div', 'mistake', message, 'position:absolute; top:50%; left: 50%; padding:20px; background:#fff; transform: translate(-50%, -50%);');
+    var mistakeClose = window.utils.createNewElement('span', 'mistake__close', 'x', 'position:absolute; top:5px; right: 5px;');
+    mistake.appendChild(mistakeClose);
+    window.setup.setup.appendChild(mistake);
+
+    var closeMistakeHandler = function () {
+      mistake.remove();
+    };
+    mistakeClose.addEventListener('click', closeMistakeHandler);
+  };
+
   window.utils = {
     getRandomInteger: getRandomInteger,
-    getRandomElement: getRandomElement
+    getRandomElement: getRandomElement,
+    createNewElement: createNewElement,
+    loadErrorHahdler: loadErrorHahdler
   };
 })();
